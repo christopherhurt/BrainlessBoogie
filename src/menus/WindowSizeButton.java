6ebe183fb372aee58ba7f823f8460db18f809104
christopherhurt <chris526@vt.edu>
2018-04-22 21:01:03 -0500

    wtf no sync

--- src/menus/WindowSizeButton.java
+++ src/menus/WindowSizeButton.java
@@ -1,8 +1,8 @@
 package menus;
 
-import main.Game;
 import main.Texture;
 import main.TexturedMenuButton;
+import main.Utils;
 
 public class WindowSizeButton extends TexturedMenuButton {
     
@@ -19,6 +19,7 @@ public class WindowSizeButton extends TexturedMenuButton {
     public void update() {
         
         if (isReleased()) {
+            Utils.MOUSE_CLICK_SOUND.play();
             Settings.setWindowWidth(windowWidth);
         }
         
